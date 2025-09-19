d358352cfde8612465a81269b59beaffccd9f424
akihide-hatano <a.h.19841202@gmail.com>
2025-09-19 16:27:26 +0900

    web.phpを編集しました

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,18 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/health', function () {
+    Log::info('health ping', [
+        'ip' => request()->ip(),
+        'ua' => request()->userAgent(),
+    ]);
+    return ['ok' => true];
 });
 
 Route::get('/dashboard', function () {
